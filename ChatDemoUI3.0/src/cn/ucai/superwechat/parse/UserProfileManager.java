ec0b1fe19770f80b961f4b2ba780c9de5b0c1d3a
huangdachui1991 <604754456@qq.com>
2016-11-01 19:05:56 +0800

    修改helper,model,application,dbmanager的前缀，修改为SuperWechat

--- ChatDemoUI3.0/src/cn/ucai/superwechat/parse/UserProfileManager.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/parse/UserProfileManager.java
@@ -4,8 +4,8 @@ import android.content.Context;
 
 import com.hyphenate.EMValueCallBack;
 import com.hyphenate.chat.EMClient;
-import cn.ucai.superwechat.DemoHelper;
-import cn.ucai.superwechat.DemoHelper.DataSyncListener;
+import cn.ucai.superwechat.SuperWechatHelper;
+import cn.ucai.superwechat.SuperWechatHelper.DataSyncListener;
 import cn.ucai.superwechat.utils.PreferenceManager;
 import com.hyphenate.easeui.domain.EaseUser;
 
@@ -77,7 +77,7 @@ public class UserProfileManager {
 				isSyncingContactInfosWithServer = false;
 				// in case that logout already before server returns,we should
 				// return immediately
-				if (!DemoHelper.getInstance().isLoggedIn()) {
+				if (!SuperWechatHelper.getInstance().isLoggedIn()) {
 					return;
 				}
 				if (callback != null) {
